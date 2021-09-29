0a8fe8b09f7d092b4201162724142ab39de24f14
Lune <lune9402@naver.com>
2021-09-29 11:43:17 +0900

    ContextUtil 추가 > 자동로그인 저장 기능 _저장된 자동로그인 여부 확인하는 작업 필요

--- app/src/main/java/com/example/sharedpreftest/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreftest/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.sharedpreftest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sharedpreftest.Utils.ContextUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
